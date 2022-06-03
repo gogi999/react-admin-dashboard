192e1987ff8c419f9e727312049e0d3da7014b53
gogi999 <g.petrovic9@gmail.com>
2022-06-03 13:03:51 +0200

    Added all files and components

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
+
+render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
